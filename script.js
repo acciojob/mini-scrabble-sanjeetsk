991077ffcdb75b8fe853e77435bff3aa1f96a488
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-01-20 10:47:53 +0530

    first change

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+let evaluatedText = document.getElementById("evaluatedText");
+let letterCount = document.getElementById("letterCount");
+
+evaluatedText.addEventListener("input", calculateLength);
+
+function calculateLength(){
+    let word = evaluatedText.value;
+    //if you want to remove space 
+    // let newWord = word.replaceAll(" ","")
+    let wordLength = word.length;
+    letterCount.innerHTML = wordLength;
+}
